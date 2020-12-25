89421fa6e31bfd173aa98f3cd42da463fabee51f
proson <mk9007@163.com>
2020-12-25 17:33:01 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,29 @@
 module.exports = {
-  title: 'MYJ - 工具站',
-  description: '工具站',
-  markdown: {
-    lineNumbers: true,
-    toc: {
-      includeLevel: [1, 2, 3],
-    },
-  },
-  themeConfig: {
-    nav: [
-      {
-        text: "指南",
-        link: "/guide/",
-      },
-      {
-        text: "工具",
-        items: [{ text: "代码生成器", link: "/" }],
-      },
-    ],
-    sidebar: {
-      
-    },
-    sidebarDepth: 3,
-  }
+	title: 'MYJ - 工具站',
+	description: '工具站',
+	markdown: {
+		lineNumbers: true,
+		toc: {
+			includeLevel: [1, 2, 3],
+		},
+	},
+	plugins: ['run'],
+	themeConfig: {
+		nav: [{
+				text: "指南",
+				link: "/guide/",
+			},
+			{
+				text: "工具",
+				items: [{
+					text: "代码生成器",
+					link: "/"
+				}],
+			},
+		],
+		sidebar: {
+
+		},
+		sidebarDepth: 3,
+	}
 }
